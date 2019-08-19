faab58a8cd85e399e95af1ba371ceb8bdf2f247a
Toshiaki Genda <prndniko@gmail.com>
2019-08-19 16:24:00 +0000

    4章4.7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,28 @@ class PostsController < ApplicationController
     @post.save
     redirect_to posts_index_url
   end
-  
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to posts_index_url
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_index_url
   end
 end
