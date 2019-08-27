0bcdcda07d1c8c89f3cd0919a4438b11b47a86be
Toshiaki Genda <prndniko@gmail.com>
2019-08-27 10:11:47 +0000

    5章5.7.3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
   def new
-    
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to posts_index_url
+    if @post.save
+      flash[:notice] = "投稿しました。"
+      redirect_to posts_index_url
+    else
+      render :new
+    end
   end
 
   def index
@@ -24,12 +28,19 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to posts_index_url
+    if @post.save
+      flash[:notice] = "投稿を編集しました。"
+      redirect_to posts_index_url
+    else
+#      redirect_to edit_post_url @post
+      render :edit
+    end
   end
+  
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました。"
     redirect_to posts_index_url
   end
 end
