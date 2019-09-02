ce7b3b448d68c23a162158f96c354aae415da8aa
Toshiaki Genda <prndniko@gmail.com>
2019-09-02 06:56:19 +0000

    6章6.9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
   def index
     @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user =User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました！"
+      redirect_to user_url @user
+    else
+      render :new
+    end
   end
 end
