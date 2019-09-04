4afd618fecce00fd8f4121773346b9c8f283e055
Toshiaki Genda <prndniko@gmail.com>
2019-09-04 16:21:08 +0000

    8章8.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,4 +44,21 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+  
+  def login_page
+    
+  end
+  
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました。"
+      redirect_to posts_index_url
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています。"
+      @email = params[:email]
+      @password = params[:password]
+      render :login_page
+    end
+  end
 end
